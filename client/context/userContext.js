b25afbecf9acf09674f81ebcfb05da4d3dda6d64
Eike-Matos <eikeadrian9830@gmail.com>
2024-11-25 17:05:15 -0300

    added render

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "https://taskfyer.onrender.com";
+  const serverUrl = "https://taskmain-dr5x.onrender.com";
 
   const router = useRouter();
 
